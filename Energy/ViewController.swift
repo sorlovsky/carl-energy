3d145555baad6134bb998f7c707faf118f179cf2
simonorlovsky <simon.orlovsky@yahoo.com>
2015-04-30 18:13:32 -0500

    First commit

---
+++ Energy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Energy
+//
+//  Created by Simon Orlovsky on 4/30/15.
+//  Copyright (c) 2015 simonorlovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
