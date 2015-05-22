87eb43584683ca7617f3f69e2f31ad60e8ca55b4
simonorlovsky <simon.orlovsky@yahoo.com>
2015-05-21 23:23:27 -0500

    Slight mods

---
+++ Energy/ConsumptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConsumptionTableViewCell.swift
+//  Energy
+//
+//  Created by Simon Orlovsky on 5/20/15.
+//  Copyright (c) 2015 simonorlovsky. All rights reserved.
+//
+
+import UIKit
+
+class ConsumptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
