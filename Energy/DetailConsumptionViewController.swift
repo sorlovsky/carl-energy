3e06cdcc5d413cea6d930c8872df28b224173314
Caleb Braun <Caleb@eduroam-164-201.dyn.carleton.edu>
2015-06-08 16:42:15 -0500

    cleaned up graphs and some code

--- Energy/DetailConsumptionViewController.swift
+++ Energy/DetailConsumptionViewController.swift
@@ -1,9 +1,9 @@
 //
 //  DetailConsumptionViewController.swift
-//  Energy
+//  Carleton Energy App
+//  6/8/2015
 //
-//  Created by Simon Orlovsky on 5/10/15.
-//  Copyright (c) 2015 simonorlovsky. All rights reserved.
+//  By Hami Abdi, Caleb Braun, and Simon Orlovsky –
 //
 
 import UIKit
@@ -14,18 +14,19 @@ class DetailConsumptionViewController: UIViewController {
     @IBOutlet weak var barGraphView: BarGraphView!
     @IBOutlet weak var energyTypeLabel: UILabel!
     @IBOutlet weak var energyUnitsLabel: UILabel!
-    @IBOutlet weak var timePeriodLabel: UILabel!
+    
+    // For implementing changing time values
+    var timePeriod = "this year"
     
     // An array of raw building data
     var buildingsDictionaries = []
     
     var selectedBuildings = [String]()
-    let energyTypeArray = ["Electricity", "Water", "Natural Gas", "Steam Condensate", "Total Energy"]
+    let energyTypeArray = ["Electricity", "Water", "Natural Gas", "Steam"]
     let timePeriodArray = ["today", "month", "year"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // The default is total energy
         getGraphData("total energy", resolution: "month")
     }
@@ -46,14 +47,51 @@ class DetailConsumptionViewController: UIViewController {
         data.fetch(self.selectedBuildings, meterType: meterType, startDate: startDate, endDate: endDate, resolution: resolution, callback: setupGraphDisplay)
     }
     
+    //Add building data to the graph
     func setupGraphDisplay(results:[String:[Double]]) {
-    
-        //Add building data to the graph
         self.barGraphView.loadData(results)
         //Indicate that the graph needs to be redrawn on the main queue
         dispatch_async(dispatch_get_main_queue()) {
             self.barGraphView.setNeedsDisplay()
         }
+    }
+    
+    func getStartDate() -> NSDate {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitYear | .CalendarUnitHour | .CalendarUnitMinute, fromDate: date)
+        let dateComponents = NSDateComponents()
+
+        // For implementing changing time values
+        switch self.timePeriod {
+        case "today":
+            println("Showing data for today!")
+        case "this month":
+            dateComponents.day = 01
+        case "this year":
+            dateComponents.year = components.year
+            dateComponents.month = 01
+            dateComponents.day = 01
+        default:
+            dateComponents.day = 01
+        }
+        let startDate = NSCalendar.currentCalendar().dateFromComponents(dateComponents)!
+        return startDate
+    }
+    
+    func updateGraphEnergyData() {
+        getGraphData(self.energyTypeLabel.text!, resolution: "month")
+        switch self.energyTypeLabel.text! {
+        case "Electricity":
+            self.energyUnitsLabel.text = "total kWh"
+        case "Water":
+            self.energyUnitsLabel.text = "total gal"
+        case "Steam":
+            self.energyUnitsLabel.text = "total kBTU"
+        default:
+            self.energyUnitsLabel.text = "total kWh"
+        }
+        self.energyTypeLabel.sizeToFit()
     }
     
     @IBAction func energyTypeBackButtonPressed(sender: AnyObject) {
@@ -75,44 +113,5 @@ class DetailConsumptionViewController: UIViewController {
             updateGraphEnergyData()
         }
     }
-    
-    func getStartDate() -> NSDate {
-        let date = NSDate()
-        let calendar = NSCalendar.currentCalendar()
-        let components = calendar.components(.CalendarUnitYear | .CalendarUnitHour | .CalendarUnitMinute, fromDate: date)
-        let dateComponents = NSDateComponents()
-
-        switch self.timePeriodLabel.text! {
-        case "today":
-            println("today!")
-        case "this month":
-            dateComponents.day = 01
-        case "this year":
-            dateComponents.year = components.year
-            dateComponents.month = 01
-            dateComponents.day = 01
-        default:
-            dateComponents.day = 01
-        }
-        let startDate = NSCalendar.currentCalendar().dateFromComponents(dateComponents)!
-        return startDate
-    }
-    
-    func updateGraphEnergyData() {
-        getGraphData(self.energyTypeLabel.text!, resolution: "month")
-        switch self.energyTypeLabel.text! {
-        case "Electricity":
-            self.energyUnitsLabel.text = "total kWh"
-        case "Water":
-            self.energyUnitsLabel.text = "total gal"
-        case "Steam Condensate":
-            self.energyUnitsLabel.text = "total kBTU"
-        case "Total Energy":
-            self.energyUnitsLabel.text = "total kBTU"
-        default:
-            self.energyUnitsLabel.text = "total kWh"
-        }
-    }
-    
 
 }
